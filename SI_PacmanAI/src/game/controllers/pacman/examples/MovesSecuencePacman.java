e7dbb557ccc944a2f8d8841483f610dbf9fbf0df
Víctor Hernández <alu0100697032@ull.edu.es>
2016-01-05 17:34:47 +0000

    MoveSecuencePacman should be fixed...

--- SI_PacmanAI/src/game/controllers/pacman/examples/MovesSecuencePacman.java
+++ SI_PacmanAI/src/game/controllers/pacman/examples/MovesSecuencePacman.java
@@ -7,22 +7,18 @@ package game.controllers.pacman.examples;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import game.PacManSimulator;
 import game.controllers.ghosts.game.GameGhosts;
 import game.controllers.pacman.PacManHijackController;
-import game.core.G;
 import game.core.Game;
 import utils.file.MovesSecuenceReader;
 
 public class MovesSecuencePacman extends PacManHijackController{
 	private int aux;
 	private ArrayList<Integer> secuence;
+	private boolean first;
 	
 	public MovesSecuencePacman() {
-		pacman.set(G.RIGHT);
 		// TODO Auto-generated constructor stub
 		try {
 			MovesSecuenceReader moveSecuenceReader = new MovesSecuenceReader();
@@ -31,24 +27,37 @@ public class MovesSecuencePacman extends PacManHijackController{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		aux = 0;
+		aux = 1;
+		first = true;
 	}
 	
 	@Override
 	public void tick(Game game, long timeDue) {	//set flag as true to include reversals	
+		//pacman.set(Game.RIGHT);
+		if(first) {
+			pacman.set(secuence.get(0));
+			first = false;
+		}
 		int[] directions = game.getPossiblePacManDirs(false);		//set flag as false to prevent reversals
 		ArrayList<Integer> list = new ArrayList<Integer>(directions.length);
 		for (int i = 0; i < directions.length; i++)
-		  list.add(Integer.valueOf(directions[i]));
+		  list.add(Integer.valueOf(directions[i]));		
+		
+		if(aux >= secuence.size())
+			aux = 0;
 		
 		if (directions.length > 1 && list.contains(secuence.get(aux))) {
 			pacman.set(secuence.get(aux));
-			System.out.println(secuence.get(aux));
-			this.aux++;
-		} 
-		
-		if(this.aux >= secuence.size())
-			this.aux = 0;
+			aux++;
+			//System.out.println(secuence.get(aux));	
+		} 	
+	}
+	
+	@Override
+	public void reset(Game game) {
+		super.reset(game);
+		aux = 1;
+		first = true;
 	}
 	
 	public static void main(String[] args) {
